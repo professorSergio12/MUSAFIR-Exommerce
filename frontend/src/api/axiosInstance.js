e02d49acef0596b9e529eaa09b6db6be07252c21
Rohit Rawat <rohitrawat12092002@gmail.com>
2025-10-23 17:24:05 +0530

    add backendURL

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    // baseURL: 'http://localhost:4000/api',
+    baseURL: 'https://musafir-exommerce.vercel.app/api',
     withCredentials: true,
 })
 
