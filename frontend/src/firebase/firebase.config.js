e56e0ebb15b5f876679be4a18b25162f0922d189
Rohit Rawat <rohitrawat12092002@gmail.com>
2025-11-28 12:44:32 +0530

    new changes

--- frontend/src/firebase/firebase.config.js
+++ frontend/src/firebase/firebase.config.js
@@ -1,6 +1,8 @@
 
 import { initializeApp } from "firebase/app";
 
+console.log(import.meta.env.VITE_FIREBASE_API_KEY);
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
