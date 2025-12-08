7fc215f5a7e0c5d6a10f247e226ef4fa95de63d5
Rohit Rawat <rohitrawat12092002@gmail.com>
2025-12-08 18:21:55 +0530

    trying to solve the timeout issue in EC2

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    // baseURL: 'http://13.51.177.114:4000/api',
-    baseURL: 'http://localhost:4000/api',
+    baseURL: 'http://13.60.224.241:4000/api',
+    // baseURL: 'http://localhost:4000/api',
     // baseURL: 'https://musafir-exommerce.vercel.app/api',
     withCredentials: true,
 })
