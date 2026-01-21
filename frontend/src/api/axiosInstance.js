9b3e98f4e771d0a02894b46a7750cfa41355b2fd
Rohit Rawat <rohitrawat12092002@gmail.com>
2026-01-21 09:38:49 +0530

    made changes in url

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: import.meta.env.VITE_API_URL_AWS,
-  baseURL: import.meta.env.VITE_APP_API_URL_LOCAL,
-  // baseURL: import.meta.env.VITE_APP_API_URL_VERCEL,
+  // baseURL: import.meta.env.VITE_APP_API_URL_LOCAL,
+  baseURL: import.meta.env.VITE_APP_API_URL_VERCEL,
   withCredentials: true,
 });
 
